7f922f3a168ec6f6853c827294042e485f01be79
Jason Wang <wanghung07@gmail.com>
2023-11-05 22:59:25 +0100

    Initial commit

---
+++ src/components/NewReminder.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react"
+
+interface NewReminderProps {
+    onAddReminder: (title: string) => void
+}
+
+const NewReminder = ({ onAddReminder }: NewReminderProps) => {
+    const [title, setTitle] = useState("")
+
+    const submitForm = (e: React.FormEvent) => {
+        e.preventDefault()
+        if (!title) return
+
+        onAddReminder(title)
+    }
+
+    return (
+        <form onSubmit={submitForm}>
+            <label htmlFor=""></label>
+            <input
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                id="title"
+                type="text"
+                className="form-control"
+            />
+            <button
+                type="submit"
+                className="btn btn-primary rounded-pill my-3
+        "
+            >
+                Add reminder
+            </button>
+        </form>
+    )
+}
+
+export default NewReminder
